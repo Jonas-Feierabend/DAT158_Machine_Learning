0433476a6cb3a4abe9b1b24c66c5846dd0d752e6
Jonas-Feierabend <jonas.feierabend1@web.de>
2023-10-28 12:06:47 +0200

    finished problem 3

--- compulsory-2/knapsack-dp.py
+++ compulsory-2/knapsack-dp.py
@@ -1,12 +1,8 @@
-import copy 
+import copy
 """config"""
 I = [[2,3], [4,3], [3,4], [3,3], [4,7]] # items (size, value) 
 B = 7  #backpack size
 remove_dominating = True
-
-
-
-
 
 """ remove dominating function"""
 def remove_dominating(A,j): 
@@ -26,52 +22,39 @@ def remove_dominating(A,j):
     A[j] = copy.deepcopy(new_A) 
         
 
-
 """initialising memory"""
 A = []# memory
-
-
 
 """start with j = 0"""
 A = [[] for _ in range(len(I)) ]
 A[0].append([0,0]) # choosing no item
 A[0].append(I[0]) # chosing the first item 
-
 print("A[",1,"]")
 print("Before: ", A[0])
 print("After: ", A[0])
 print("\n") 
- 
 
 """consider more items """
 for j in range(1,len(I)):
     A[j] = copy.deepcopy(A[j-1])# you can choose to not take the new item 
-
-
     #you can combine the new item (with no other item or with a combination) 
     for pair in A[j-1]:
-   
         new_size = pair[0] + I[j][0] # add weight of old subset and new item
         if(new_size <= B): # does it fit into backpack?
             #add new subset
             new_set = [new_size, pair[1] + I[j][1]] # combine new item with old subset
             A[j].append(new_set)
-
-
-
     #remove dominating
     print("A[",j+1,"]")
     print("Before: ", A[j])
     remove_dominating(A, j)
     print("After: ", A[j])
     print("\n")
-
-
+    
 best = 0
 for e in A[-1]:
     if(e[1] > best):
         best = e[1]
-
 print("Best result is: ", best) 
 
 
